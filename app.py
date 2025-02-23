f6d63df17ab7d6259e5c1cc7dd68117cd93d3797
Anush Sharma <anushsharma13cric@gmail.com>
2025-02-23 13:00:52 +0530

    Email-Bot

--- app.py
+++ app.py
@@ -111,4 +111,4 @@ if __name__ == "__main__":
     if not os.path.exists("uploads"):
         os.makedirs("uploads")
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
